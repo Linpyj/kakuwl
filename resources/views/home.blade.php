0dd82d39241c900ed9e8bcede43056215b32253b
rinLyn <rintaro_fujii@posiwill.co.jp>
2021-05-09 16:32:33 +0900

    create migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('MyPage') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,10 +13,20 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
+                    @if (Auth::user())
+                        {{ Auth::user()->name }}です！
+                    @endif
+                </div>
+                <div class="card-footer">
+                    {{ Auth::user() }}
                 </div>
             </div>
+            <form action="{{ route('picture.store') }}" method="post">
+                <button class="btn btn-primary">絵を投稿する</btn>
+                <!-- <div class="postbutton mt-5">
+                    <button class="btn btn-primary">絵を投稿する</button>
+                </div> -->
+            </form>
         </div>
     </div>
 </div>
